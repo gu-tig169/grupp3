10200aa31eb57eb0ac441ece3eda9f473df2e3be
wmwilliamson <49144293+wmwilliamson@users.noreply.github.com>
2020-12-14 11:18:39 +0100

    Add files via upload
    text sökfält

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'RestaurantListView.dart';
+import 'TextView.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Restaurant Finder',
-      //Ändra denna rad till den klass ni vill visa. Just nu visar den RestaurantListView.dart
-      //Ni behöver inte pusha main.dart innan vi har länkat ihop allting
-      home: RestaurantListView(),
+      home: TextView(),
     );
   }
 }
