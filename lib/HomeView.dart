f3bdea481deed5f44524d32c6471e292a7073ac4
amandafalken <73556606+amandafalken@users.noreply.github.com>
2020-12-18 13:26:59 +0100

    HomeView, med tab bar

---
+++ lib/HomeView.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text('Hitta restaurang'),
+                centerTitle: true,
+                backgroundColor: Colors.blue,
+                elevation: 0,
+                bottom: TabBar(
+                    unselectedLabelColor: Colors.white,
+                    indicatorPadding: EdgeInsets.only(left: 30, right: 30),
+                    indicator: ShapeDecoration(
+                        color: Colors.blueAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                            side: BorderSide(
+                              color: Colors.white,
+                            ))),
+                    tabs: [
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("KARTA"),
+                        ),
+                      ),
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("SÖK"),
+                        ),
+                      ),
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("FAVORITER"),
+                        ),
+                      ),
+                    ]),
+              ),
+              body: TabBarView(children: [
+                Icon(Icons.location_city),
+                Icon(Icons.search),
+                Icon(Icons.favorite),
+              ]),
+            )));
+  }
+}
